2048242f3a2839e404a27c6f5a43892eafe6e1cf
Oana Stroie <oana.stroie@hootsuite.com>
2018-07-15 15:39:27 +0300

    Add default case flow

---
+++ pkg/scheduler/algorithm/reschedule_algorithm/default_rescheduler.go
@@ -0,0 +1,189 @@
+package reschedule_algorithm
+
+import (
+	"fmt"
+	apiv1 "k8s.io/api/core/v1"
+	schedulercache "k8s.io/kubernetes/pkg/scheduler/cache"
+	"sort"
+	resource_api "k8s.io/kubernetes/pkg/api/v1/resource"
+	"github.com/golang/glog"
+	"k8s.io/kubernetes/pkg/scheduler/algorithm"
+	"k8s.io/kubernetes/pkg/scheduler/core"
+)
+
+type defaultRescheduler struct {
+	cache schedulercache.Cache
+	cachedNodeInfoMap        map[string]*schedulercache.NodeInfo
+}
+
+// nodeScore contains node and score ...
+type nodeInfoScore struct {
+	// based on how much pod will reduce capacity imbalance for a node (bigger value is better in this case)
+	score float64
+	nodeInfo   *schedulercache.NodeInfo
+}
+
+func NewDefaultRescheduleAlgorithm(cache schedulercache.Cache) algorithm.RescheduleAlgorithm {
+	return &defaultRescheduler{
+		cache: cache, cachedNodeInfoMap: make(map[string]*schedulercache.NodeInfo),
+	}
+}
+
+// TODO- move parts of this to common
+func (rescheduler *defaultRescheduler) Schedule(pod *apiv1.Pod, nodeLister algorithm.NodeLister) ([]*algorithm.PodNodeAssignment, error) {
+	nodes, err := nodeLister.List()
+	fmt.Printf("Nr of listed nodes %d\n", len(nodes))
+	glog.V(4).Infof("[Oana][Rescheduler] Nr of listed nodes%d\n", len(nodes))
+	if err != nil {
+		return []*algorithm.PodNodeAssignment{}, err
+	}
+	if len(nodes) == 0 {
+		return []*algorithm.PodNodeAssignment{}, core.ErrNoNodesAvailable
+	}
+
+	// Used for all fit and priority funcs.
+	err = rescheduler.cache.UpdateNodeNameToInfoMap(rescheduler.cachedNodeInfoMap)
+	if err != nil {
+		fmt.Println("Error while updating mapping")
+		glog.V(4).Infof("[Oana][Rescheduler] Error while updating mapping %v", err)
+		return []*algorithm.PodNodeAssignment{}, err
+	}
+
+	// Get nodeinfos from cache
+	nodeInfos := make([]*schedulercache.NodeInfo, len(nodes))
+	// Used to make diff
+	podToNodeBefore := make(map[*apiv1.Pod]string)
+	for i := range(nodes) {
+		nodeName := nodes[i].Name
+		nodeInfo := rescheduler.cachedNodeInfoMap[nodeName]
+		nodeInfos[i] = nodeInfo
+		for _, nodePod := range(nodeInfo.Pods()) {
+			podToNodeBefore[nodePod] = nodeInfo.Node().Name
+		}
+	}
+
+	newNodeInfos, err := rescheduler.Reschedule(pod, nodeInfos)
+	if err != nil {
+		fmt.Println("Error while rescheduling")
+		glog.V(4).Infof("[Oana][Rescheduler] Error while rescheduling %v", err)
+		return []*algorithm.PodNodeAssignment{}, err
+	}
+
+	assignments := make([]*algorithm.PodNodeAssignment, 0)
+	for _, nodeInfo := range(newNodeInfos) {
+		for _, pod := range(nodeInfo.Pods()) {
+			beforeNode, ok := podToNodeBefore[pod]
+			// We want to reassign pods that now should be scheduled on a different machine that they were running on
+			// or if the pod was not assigned yet
+			if !ok || nodeInfo.Node().Name != beforeNode {
+				assignments = append(assignments, &algorithm.PodNodeAssignment{Pod: pod, SelectedMachine: nodeInfo.Node().Name})
+			}
+		}
+	}
+	return assignments, nil
+}
+
+
+func (rescheduler *defaultRescheduler) Reschedule(pod *apiv1.Pod, existingNodes []*schedulercache.NodeInfo) ([]*schedulercache.NodeInfo, error) {
+	// Create list with all pods
+	pods := make([]*apiv1.Pod, 0)
+	for _, nodeInfo := range(existingNodes) {
+		for _, nodePod := range(nodeInfo.Pods()) {
+			if nodePod.Namespace == pod.Namespace{
+				// We only reschedule pods in the same namespace
+				pods = append(pods, nodePod)
+			}
+		}
+	}
+	// Also add the pod to be scheduled
+	pods = append(pods, pod)
+	fmt.Printf("Nr of pods to reschedule %d\n", len(pods))
+	glog.V(4).Infof("[Oana][Rescheduler] Nr of pods to reschedule %d\n", len(pods))
+
+	// Create a list with empty nodes from the list of existing nodes
+	nodesInfos := make([]*schedulercache.NodeInfo, 0)
+	for _, nodeInfo := range(existingNodes) {
+		newNodeInfo := schedulercache.NewNodeInfo()
+		newNodeInfo.SetNode(nodeInfo.Node())
+		nodesInfos = append(nodesInfos, newNodeInfo)
+	}
+	fmt.Printf("Nr of nodes %d\n", len(nodesInfos))
+	glog.V(4).Infof("[Oana][Rescheduler] Nr of nodes %d\n", len(nodesInfos))
+
+	// Taken from https://github.com/kubernetes/autoscaler/blob/master/cluster-autoscaler/estimator/binpacking_estimator.go#L60
+	// nodeWithPod function returns NodeInfo, which is a copy of nodeInfo argument with an additional pod scheduled on it.
+	nodeWithPod := func(nodeInfo *schedulercache.NodeInfo, pod *apiv1.Pod) *schedulercache.NodeInfo {
+		podsOnNode := nodeInfo.Pods()
+		podsOnNode = append(podsOnNode, pod)
+		newNodeInfo := schedulercache.NewNodeInfo(podsOnNode...)
+		newNodeInfo.SetNode(nodeInfo.Node())
+		return newNodeInfo
+	}
+
+	// Sort pods by dimension (multiply resources) - we want to schedule biggest pods first
+	sort.Slice(pods, func(i, j int) bool { return multiplyPodResourceReq(pods[i]) > multiplyPodResourceReq(pods[j])})
+	for _, pod := range(pods) {
+		fmt.Printf("[Oana][Rescheduler] Trying to reschdule pod: %s (%v, %v)", pod.ObjectMeta.UID,
+			resource_api.GetResourceRequest(pod, apiv1.ResourceCPU),
+			resource_api.GetResourceRequest(pod, apiv1.ResourceMemory))
+		nodeInfoScores := rescheduler.score(pod, nodesInfos)
+		if len(nodeInfoScores) == 0 {
+			// Pod does not fit on any node
+			return []*schedulercache.NodeInfo{}, fmt.Errorf("Pod does not fit on any node")
+		}
+		biggestScore := nodeInfoScores[0].score
+		biggestScoreNodeInfo := nodeInfoScores[0].nodeInfo
+		for _, nodeInfoScore := range(nodeInfoScores){
+			if nodeInfoScore.score > biggestScore {
+				biggestScore = nodeInfoScore.score
+				biggestScoreNodeInfo = nodeInfoScore.nodeInfo
+			}
+		}
+		// Update the node info with the new pod
+		for i, nodeInfo := range nodesInfos {
+			if nodeInfo == biggestScoreNodeInfo {
+				nodeInfo := nodeWithPod(nodeInfo, pod)
+				nodesInfos[i] = nodeInfo
+			}
+		}
+	}
+
+	// Return the new scheduling (nodes with pods)
+	return nodesInfos, nil
+}
+
+func (rescheduler *defaultRescheduler) score(pod *apiv1.Pod, nodeInfos []*schedulercache.NodeInfo) []*nodeInfoScore {
+	fmt.Println("[Oana][Rescheduler] \tScoring nodes:")
+	glog.V(4).Infof("[Oana][Rescheduler] \tScoring nodes:")
+	nodeInfoScores := make([]*nodeInfoScore, 0)
+	// TODO parallelize
+	for _, nodeInfo := range nodeInfos {
+		// We drop pods that can't be scheduled on the node.
+		if ! podFitsNode(pod, nodeInfo) {
+			fmt.Println("\t[Oana][Rescheduler] \tPod does not fit node, Can't schedule it on this node")
+			glog.V(4).Infof("[Oana][Rescheduler] \tPod does not fit node, Can't schedule it on this node")
+			continue
+		}
+		// TODO - nicer way of calculating this. Maybe more generic like the priorities
+		// It is more important to keep the nodes low loaded than perfectly balanced
+		nodeScore := getPodOnNodeImbalanceScore(pod, nodeInfo) + 2*getPodOnNodeFreenessScore(pod, nodeInfo)
+		// Nodes that have no resources allocated yet are preferred
+		if noPodsOnNode(nodeInfo) {
+			// TODO prettier way
+			nodeScore += 1
+		}
+		nodeInfoScores = append(nodeInfoScores, &nodeInfoScore{
+			score: nodeScore,
+			nodeInfo:   nodeInfo,
+		})
+	}
+	return nodeInfoScores
+}
+
+func multiplyPodResourceReq(pod *apiv1.Pod) int64 {
+	return resource_api.GetResourceRequest(pod, apiv1.ResourceCPU) *resource_api.GetResourceRequest(pod, apiv1.ResourceMemory)
+}
+
+func noPodsOnNode(nodeInfo *schedulercache.NodeInfo) bool {
+	return len(nodeInfo.Pods()) == 0
+}
--- pkg/scheduler/algorithm/reschedule_algorithm/main/main.go
+++ pkg/scheduler/algorithm/reschedule_algorithm/main/main.go
@@ -15,7 +15,11 @@ import (
 
 func main() {
 	fmt.Println("hello world")
+	//runClusterAutoscalerExample()
+	runDefaultExample()
+}
 
+func runClusterAutoscalerExample() {
 	/*
 	2 nodes: N1 (2CPUs, 4GB) and N2 (4CPUs, 4GB)
 		N1 name : disertatie
@@ -35,8 +39,10 @@ func main() {
 	pod2_1.Spec.NodeName = "Node2 (3000, 2000)"
 	pod2_1.ObjectMeta.UID = "pod2_1"
 	pod2_2 := newResourcePod(schedulercache.Resource{MilliCPU: 750, Memory: 1000})
-	pod2_2.Spec.NodeName = "Node2 (3000, 2000)"
-	pod2_2.ObjectMeta.UID = "pod2_2"
+	//pod2_2.Spec.NodeName = "Node2 (3000, 2000)"
+	pod2_2.Spec.NodeName = "Node1 (1500, 3500)"
+	//pod2_2.ObjectMeta.UID = "pod2_2"
+	pod2_2.ObjectMeta.UID = "pod1_2"
 
 	node1 := v1.Node{ObjectMeta: metav1.ObjectMeta{Name: "Node1 (1500, 3500)", },
 		Status: v1.NodeStatus{Allocatable: makeAllocatableResources(1500, 3500)}}
@@ -58,12 +64,69 @@ func main() {
 	fmt.Printf("err 4 %v", err)
 	err = cache.AddPod(pod2_2)
 	fmt.Printf("err 5 %v", err)
-	rescheduler := reschedule_algorithm.NewRescheduleAlgorithm(cache)
+	rescheduler := reschedule_algorithm.NewClusterAutoscalerRescheduleAlgorithm(cache)
 	nodeLister := schedulertesting.FakeNodeLister([]*v1.Node{&node1, &node2})
 
 	assignments, _ := rescheduler.Schedule(pod1_2, nodeLister)
 	for _, assignment := range(assignments) {
-		fmt.Printf("Node %s - pod (%v %v)", assignment.SelectedMachine,
+		fmt.Printf("Node %s - pod %s (%v %v)", assignment.SelectedMachine,
+			assignment.Pod.ObjectMeta.UID,
+			resource_api.GetResourceRequest(assignment.Pod, v1.ResourceCPU),
+			resource_api.GetResourceRequest(assignment.Pod, v1.ResourceMemory))
+		fmt.Println()
+
+	}
+	fmt.Println("Reschedule ended")
+}
+
+
+func runDefaultExample() {
+	/*
+	2 nodes: N1 (2CPUs, 4GB) and N2 (4CPUs, 4GB)
+		N1 name : disertatie
+		N2 name : dissertation
+	2 types of pods with limit requests:
+		P1 0.5CPU and 1GB
+		P2 2CPU and 1GB
+
+	 P1, P1, P2
+	 */
+
+	pod1_1 := newResourcePod(schedulercache.Resource{MilliCPU: 500, Memory: 1000})
+	pod1_1.Spec.NodeName = "Node1 (2000, 4000)"
+	pod1_1.ObjectMeta.UID = "pod1_1"
+	pod1_2 := newResourcePod(schedulercache.Resource{MilliCPU: 500, Memory: 1000})
+	pod1_2.Spec.NodeName = "Node2 (4000, 4000)"
+	pod1_2.ObjectMeta.UID = "pod1_2"
+
+	pod2_1 := newResourcePod(schedulercache.Resource{MilliCPU: 2000, Memory: 1000})
+	pod2_1.ObjectMeta.UID = "pod2_1"
+
+	node1 := v1.Node{ObjectMeta: metav1.ObjectMeta{Name: "Node1 (2000, 4000)", },
+		Status: v1.NodeStatus{Allocatable: makeAllocatableResources(2000, 4000)}}
+
+	node2 := v1.Node{ObjectMeta: metav1.ObjectMeta{Name: "Node2 (4000, 4000)", },
+		Status: v1.NodeStatus{Allocatable: makeAllocatableResources(4000, 4000)}}
+
+	cache := schedulercache.New(time.Duration(0), wait.NeverStop)
+
+	err := cache.AddNode(&node1)
+	fmt.Printf("err 1 %v", err)
+	err = cache.AddNode(&node2)
+	fmt.Printf("err 2 %v", err)
+
+	err = cache.AddPod(pod1_1)
+	fmt.Printf("err 3 %v", err)
+	//cache.AddPod(pod1_2)
+	err = cache.AddPod(pod1_2)
+	fmt.Printf("err 5 %v", err)
+	rescheduler := reschedule_algorithm.NewDefaultRescheduleAlgorithm(cache)
+	nodeLister := schedulertesting.FakeNodeLister([]*v1.Node{&node1, &node2})
+
+	assignments, _ := rescheduler.Schedule(pod2_1, nodeLister)
+	for _, assignment := range(assignments) {
+		fmt.Printf("Node %s - pod %s (%v %v)", assignment.SelectedMachine,
+			assignment.Pod.ObjectMeta.UID,
 			resource_api.GetResourceRequest(assignment.Pod, v1.ResourceCPU),
 			resource_api.GetResourceRequest(assignment.Pod, v1.ResourceMemory))
 		fmt.Println()
